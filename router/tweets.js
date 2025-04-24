973fc14f9170920af8740f851e65d05a14ce2bc2
Seokho <seokho0120@gmail.com>
2025-04-25 00:19:21 +0900

    feat: Get tweets 확인

--- router/tweets.js
+++ router/tweets.js
@@ -1,5 +1,30 @@
 import express from "express";
 
+const tweets = [
+  {
+    id: "1",
+    text: "첫번쨰 트윗",
+    createdAt: Date.now().toString(),
+    name: "seokho",
+    username: "seokori",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+];
+
 const router = express.Router();
 
+// GET /tweets
+// GET /tweets?username=:username
+router.get("/", (req, res, next) => {
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((t) => t.username === username)
+    : tweets;
+  res.status(200).json(data);
+});
+
+// GET /tweets/:id
+// POST /tweets/:id
+// PUT /tweets/:id
+// DELETE /tweets/:id
 export default router;

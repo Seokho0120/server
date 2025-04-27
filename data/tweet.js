49b8a2b45fbf1857fdcbdca52056b0dbe2784144
Seokho <seokho0120@gmail.com>
2025-04-27 18:24:03 +0900

    feat: 서버에 data층 만들기

---
+++ data/tweet.js
@@ -0,0 +1,55 @@
+let tweets = [
+  {
+    id: "1",
+    text: "첫번쨰 트윗",
+    createdAt: Date.now().toString(),
+    name: "seokho",
+    username: "seokori",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+  {
+    id: "2",
+    text: "하이!",
+    createdAt: Date.now().toString(),
+    name: "Bob",
+    username: "bob",
+  },
+];
+
+export function getAll() {
+  return tweets;
+}
+
+export function getAllByUsername(username) {
+  return tweets.filter((tweet) => tweet.username === username);
+}
+
+export function getById(id) {
+  return tweets.find((tweet) => tweet.id === id);
+}
+
+export function create(text, name, username) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createAt: new Date(),
+    name,
+    username,
+  };
+
+  tweets = [tweet, ...tweets];
+  return tweet;
+}
+
+export function update(id, text) {
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+  }
+
+  return tweet;
+}
+
+export function remove(id) {
+  tweets.filter((tweet) => tweet.id !== id);
+}
--- router/tweets.js
+++ router/tweets.js
@@ -1,25 +1,5 @@
 import express from "express";
-
-// 현재는 서버 메모리에 배열로 관리하고 있는거임
-// tweets는 DB 사용전 임시로 저장하고 있음, 실제로 이렇게 사용하면 매우 좋지 않음
-
-let tweets = [
-  {
-    id: "1",
-    text: "첫번쨰 트윗",
-    createdAt: Date.now().toString(),
-    name: "seokho",
-    username: "seokori",
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
-  },
-  {
-    id: "2",
-    text: "하이!",
-    createdAt: Date.now().toString(),
-    name: "Bob",
-    username: "bob",
-  },
-];
+import * as tweetRepository from "../data/tweet.js";
 
 const router = express.Router();
 
@@ -28,15 +8,15 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const username = req.query.username;
   const data = username
-    ? tweets.filter((tweet) => tweet.username === username)
-    : tweets;
+    ? tweetRepository.getAllByUsername(username)
+    : tweetRepository.getAll();
   res.status(200).json(data);
 });
 
 // GET /tweets/:id
 router.get("/:id", (req, res, next) => {
   const id = req.params.id;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+  const tweet = tweetRepository.getById(id);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
@@ -47,15 +27,7 @@ router.get("/:id", (req, res, next) => {
 // POST /tweets/:id
 router.post("/", (req, res, next) => {
   const { text, name, username } = req.body;
-  const tweet = {
-    id: Date.now().toString(), // 나중에 DB사용하면 자동으로 고유한 id를 만들 수 있음
-    text,
-    createAt: new Date(),
-    name,
-    username,
-  };
-
-  tweets = [tweet, ...tweets];
+  const tweet = tweetRepository.create(text, name, username);
   res.status(201).json(tweet);
 });
 
@@ -63,10 +35,9 @@ router.post("/", (req, res, next) => {
 router.put("/:id", (req, res, next) => {
   const id = req.params.id;
   const text = req.body.text;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+  const tweet = tweetRepository.update(id, text);
 
   if (tweet) {
-    tweet.text = text;
     res.status(200).json(tweet);
   } else {
     res.status(404).json({ message: `Tweet id(${id}) not found` });
@@ -76,7 +47,7 @@ router.put("/:id", (req, res, next) => {
 // DELETE /tweets/:id
 router.delete("/:id", (req, res, next) => {
   const id = req.params.id;
-  tweets = tweets.filter((tweet) => tweet.id !== id);
+  tweetRepository.remove(id);
   res.sendStatus(204);
 });
 export default router;

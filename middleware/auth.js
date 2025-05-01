3e84387087a3c655adc8fe1ce78b8020418c509e
Seokho <seokho0120@gmail.com>
2025-05-02 00:32:26 +0900

    fix: isAuth 수정

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,19 +5,21 @@ const AUTH_ERROR = { message: "Authorization Error" };
 
 export async function isAuth(req, res, next) {
   const authHeader = req.get("Authorization");
-  if (!authHeader && authHeader.startsWith("Bearer ")) {
+  if (!(authHeader && !authHeader.startsWith("Bearer "))) {
     return res.status(401).json(AUTH_ERROR);
   }
 
   const token = authHeader.split(" ")[1];
   jwt.verify(
     token,
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE3NDYxMTIxNDQzODQiLCJpYXQiOjE3NDYxMTIxNDQsImV4cCI6MTc0NjI4NDk0NH0.fLMz5hISlpx7AaSINqKtroOLHWAuDnAxh-pc1Y_q08U",
+    "a3d4595afbd6beb421c7902f40ed1540",
     async (error, decoded) => {
       if (error) {
+        console.log("error", error);
         return res.status(401).json(AUTH_ERROR);
       }
       const user = await userRepository.findById(decoded.id);
+      user.log;
       if (!user) {
         return res.status(401).json(AUTH_ERROR);
       }

89fed7920f2292d2bc49d70394ff918cc67c228c
Seokho <seokho0120@gmail.com>
2025-05-02 00:04:08 +0900

    feat: bob일때도 로그인 확인, 해시값으로 수정

--- data/auth.js
+++ data/auth.js
@@ -1,10 +1,9 @@
-import { url } from "inspector";
 // abcd1234: $2b$12$0v1x5j4z3Q6Z5Y8g7G9c1OaXk5J6e4q3F5Z5e5e5e5e5e5e5e5e5e
 let users = [
   {
     id: 1,
     username: "bob",
-    password: "abcd1234",
+    password: "$2b$12$eFJz4QzmTLujYOodXiMtV.KCGhr6l0Fq0dOTzr0KSmeH9pcE8OS1e",
     name: "Bob",
     email: "bob@gmail.com",
     url: "https://bob.com",

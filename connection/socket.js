ce717aa61707f99d4ef5ae48203ceee128e1662b
Seokho <seokho0120@gmail.com>
2025-05-09 00:47:13 +0900

    docs: token은 반드시 handshake.auth.token에 담겨야 함

--- connection/socket.js
+++ connection/socket.js
@@ -11,6 +11,12 @@ class Socket {
     });
 
     this.io.use((socket, next) => {
+      // token은 반드시 handshake.auth.token에 담겨야 함
+      // 그 이유는 보안상으로 socket.io는 cors를 지원하지 않음
+      // cors를 지원하지 않기 때문에, 클라이언트에서 socket.io를 사용할 때 반드시 auth를 통해서 인증을 해야 함
+      // auth를 통해서 인증을 하지 않으면, socket.io는 연결을 거부함
+      // 따라서, auth를 통해서 인증을 하지 않으면, socket.io는 연결을 거부함
+
       const token = socket.handshake.auth.token;
       if (!token) {
         return next(new Error("Authentication error"));
